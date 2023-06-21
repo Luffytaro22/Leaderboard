8df11af5f13f49fd676aac8900fd004e3776bd9c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-21 11:09:46 -0500

    Modified the createScores function to gather info of the api

--- modules/storage.js
+++ modules/storage.js
@@ -1,10 +1,37 @@
-function createScores(scores) {
-  scores = localStorage.getItem('scores') ? JSON.parse(localStorage.getItem('scores')) : [];
-  return scores;
+async function createScores() {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Z3iGcEUE1HlerNd8QYix/scores/', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Error al obtener los puntajes');
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+    return []; // Devuelve un arreglo vacío en caso de error
+  }
 }
 
 function storage(scores) {
-  localStorage.setItem('scores', JSON.stringify(scores));
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Z3iGcEUE1HlerNd8QYix/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: `${scores.user}`,
+      score: `${scores.score}`,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json);
 }
 
 export { createScores, storage };
