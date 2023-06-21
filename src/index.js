853baf128ddc825454ac1810655a34d459c45bb6
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-21 11:31:45 -0500

    Added an async function to display the elements

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import { createScores } from '../modules/storage.js';
 import addScore from '../modules/addScore.js';
 
 // Display the scores in the table.
-let score = [];
-score = createScores(score);
-score.forEach((obj) => {
-  addScore(obj);
-});
+async function loadScores() {
+  const scores = await createScores();
+
+  scores.result.forEach((obj) => {
+    addScore(obj);
+  });
+}
+
+loadScores();
 
 form.addEventListener('submit', newScore);
